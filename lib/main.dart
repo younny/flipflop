f7e82ca24f12601a74bf001605e0e16b564dc9a9
younny <dooyoung@favoritemedium.com>
2019-02-15 15:11:33 +0900

    Set localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:flip/blocs/card_bloc.dart';
 import 'package:flip/components/home_page.dart';
@@ -27,22 +28,35 @@ class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cardBloc = Provider.of<CardBloc>(context);
-
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.amber,
-          disabledColor: Colors.grey
-        ),
-        fontFamily: 'LuckiestGuy',
-        primarySwatch: Colors.amber,
-      ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('ko', 'KR')
+      ],
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: HomePage()
+          backgroundColor: Colors.blueGrey,
+          body: HomePage()
       ),
+      builder: (BuildContext context, Widget widget) {
+        final lang = Localizations.localeOf(context).languageCode;
+        return Theme(
+          data: ThemeData(
+            buttonTheme: ButtonThemeData(
+                buttonColor: Colors.amber,
+                disabledColor: Colors.grey
+            ),
+            fontFamily: lang == 'ko' ? 'DoHyeon' : 'LuckiestGuy',
+            primarySwatch: Colors.amber,
+          ),
+          child: widget,
+        );
+      }
     );
   }
 }
