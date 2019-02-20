6cfe5b38a3dce0e478b9ce2961f8e6aabedbd2a6
younny <dooyoung@favoritemedium.com>
2019-02-20 14:10:53 +0900

    Add pronunciation property

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,24 +1,27 @@
 class WordViewModel {
   final String word;
   final String meaning;
+  final String pronunciation;
   final String category;
   final String created;
-  final String locale;
+  final String lang;
 
   WordViewModel({
     this.word,
     this.meaning,
+    this.pronunciation,
     this.category,
     this.created,
-    this.locale
+    this.lang
   });
 
   WordViewModel.fromJson(Map json)
   : word = json['word'],
     meaning = json['meaning'],
+    pronunciation = json['pronunciation'],
     category = json['catetory'],
     created = json['created'],
-    locale = json['locale'];
+    lang = json['lang'];
 
 }
 
@@ -40,55 +43,81 @@ final List<WordViewModel> mockCards = [
   WordViewModel(
     category: 'fruit',
     created: '14 Feb 2019',
-    word: 'Apple',
-    meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-        'Apple trees are cultivated woworldwideworldwideworldwideworldwiderldwidewoworldwideworldwideworldwideworldwiderldwidewoworldwideworldwideworldwideworldwiderldwidewoworldwideworldwideworldwideworldwiderldwidewoworldwideworldwideworldwideworldwiderldwidewoworldwideworldwideworldwideworldwiderldwide and are the most widely grown species in the genus Malus. '
-        'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-    locale: 'en'
+    word: '사과',
+    meaning: 'Apple',
+    pronunciation: 'sa-g-wa',
+    lang: 'ko'
   ),
   WordViewModel(
       category: 'fruit',
       created: '14 Feb 2019',
-      word: 'Melon',
-      meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-          'Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. '
-          'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-      locale: 'en'
+      word: '멜론',
+      meaning: 'Melon',
+      pronunciation: 'mael-ron',
+      lang: 'ko'
   ),
   WordViewModel(
       category: 'fruit',
       created: '14 Feb 2019',
-      word: 'Banana',
-      meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-          'Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. '
-          'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-      locale: 'en'
+      word: '바나나',
+      meaning: 'Banana',
+      pronunciation: 'ba-na-na',
+      lang: 'ko'
   ),
   WordViewModel(
       category: 'fruit',
       created: '14 Feb 2019',
-      word: 'Orange',
-      meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-          'Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. '
-          'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-      locale: 'en'
+      word: '키위',
+      meaning: 'Kiwi',
+      pronunciation: 'ki-wi',
+      lang: 'ko'
   ),
   WordViewModel(
       category: 'fruit',
       created: '14 Feb 2019',
-      word: 'Kiwi',
-      meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-          'Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. '
-          'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-      locale: 'en'
+      word: '수박',
+      meaning: 'Watermelon',
+      pronunciation: 'soo-bak',
+      lang: 'ko'
   ),
   WordViewModel(
       category: 'fruit',
       created: '14 Feb 2019',
-      word: 'Grape',
-      meaning: 'An apple is a sweet, edible fruit produced by an apple tree. '
-          'Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. '
-          'The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.',
-      locale: 'en'
+      word: '포도',
+      meaning: 'Grape',
+      pronunciation: 'po-do',
+      lang: 'ko'
+  ),
+  WordViewModel(
+      category: 'fruit',
+      created: '14 Feb 2019',
+      word: '체리',
+      meaning: 'Cherry',
+      pronunciation: 'chae-lee',
+      lang: 'ko'
+  ),
+  WordViewModel(
+      category: 'fruit',
+      created: '14 Feb 2019',
+      word: '귤',
+      meaning: 'Tangerin',
+      pronunciation: 'gyu-eul',
+      lang: 'ko'
+  ),
+  WordViewModel(
+      category: 'fruit',
+      created: '14 Feb 2019',
+      word: '망고',
+      meaning: 'Mango',
+      pronunciation: 'mang-go',
+      lang: 'ko'
+  ),
+  WordViewModel(
+      category: 'fruit',
+      created: '14 Feb 2019',
+      word: '배',
+      meaning: 'Pear',
+      pronunciation: 'bae',
+      lang: 'ko'
   )
 ];
