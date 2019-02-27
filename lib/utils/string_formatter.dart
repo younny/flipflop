25068a1ddfc472cf1b7854358ba89b9d80a4d90e
younny <younny@gmail.com>
2019-02-27 15:40:24 +0900

    Check null

--- lib/utils/string_formatter.dart
+++ lib/utils/string_formatter.dart
@@ -1,6 +1,7 @@
 class StringFormatter {
   static final String TAG_UNAVAILABLE = 'unavailable';
   static String formatMeaning(String source) {
+    if(source == null || source.length == 0) return ': ' + TAG_UNAVAILABLE;
     return ': ' + source;
   }
 }
