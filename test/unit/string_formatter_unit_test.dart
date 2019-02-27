c1b8ad060dbe1e657a4ad4e806ce8c7585e14c88
younny <younny@gmail.com>
2019-02-27 16:26:40 +0900

    Set up all cases of formatting cases

--- test/unit/string_formatter_unit_test.dart
+++ test/unit/string_formatter_unit_test.dart
@@ -3,6 +3,27 @@ import 'package:test/test.dart';
 import 'package:flip/utils/string_formatter.dart';
 
 void main() {
+  test("format word with some value", () {
+    String source = "apple";
+    String result = StringFormatter.formatWord(source);
+
+    expect(result, equals(source));
+  });
+
+  test("format word with null value", () {
+    String source;
+    String result = StringFormatter.formatWord(source);
+
+    expect(result, equals(StringFormatter.TAG_UNAVAILABLE));
+  });
+
+  test("format word with empty value", () {
+    String source = "";
+    String result = StringFormatter.formatWord(source);
+
+    expect(result, equals(StringFormatter.TAG_UNAVAILABLE));
+  });
+
   test("format meaning with some value", () {
     String source = "This is test meaning.";
     String result = StringFormatter.formatMeaning(source);
@@ -22,5 +43,26 @@ void main() {
     String result = StringFormatter.formatMeaning(source);
 
     expect(result, equals(': ${StringFormatter.TAG_UNAVAILABLE}'));
+  });
+
+  test("format pronunciation with some value", () {
+    String source = "sa-g-wa";
+    String result = StringFormatter.formatPronunciation(source);
+
+    expect(result, equals('[$source]'));
+  });
+
+  test("format pronunciation with null value", () {
+    String source;
+    String result = StringFormatter.formatPronunciation(source);
+
+    expect(result, equals('[${StringFormatter.TAG_UNAVAILABLE}]'));
+  });
+
+  test("format pronunciation with empty value", () {
+    String source = "";
+    String result = StringFormatter.formatPronunciation(source);
+
+    expect(result, equals('[${StringFormatter.TAG_UNAVAILABLE}]'));
   });
 }
