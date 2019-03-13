490ad00d951163ee7ce0cc22d0c2cfb553bac509
younny <younny@gmail.com>
2019-03-13 11:52:32 +0900

    Use custom DropdownDialog

--- lib/components/bottom_bar.dart
+++ lib/components/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flipflop/components/dropdown_dialog.dart';
 import 'package:flipflop/components/scroll_indicator.dart';
 import 'package:flipflop/pages/settings_page.dart';
 import 'package:flutter/material.dart';
@@ -77,115 +78,17 @@ class _BottomBarState extends State<BottomBar> {
   }
 
   Future<String> _showAddToMyStackAlert(BuildContext context) {
-    List<String> folders = [];
-    String selectedFolder;
-    int selectedIndex = 0;
+    List<String> folders = ["test1", "test2"];
     return showDialog<String>(
       context: context,
-      barrierDismissible: false,
+      barrierDismissible: true,
       builder: (BuildContext context) {
-        int index = 0;
-
-        return AlertDialog(
-          title: Text("Add to my stack"),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                DropdownButton<int>(
-                  value: folders.length == 0 ? null : selectedIndex,
-                  items: folders.map((folderName) {
-                    return DropdownMenuItem(
-                      value: index++,
-                      child: Text(folderName),
-                    );
-                  }).toList()
-                  ..add(DropdownMenuItem(
-                    value: folders.length,
-                    child: Row(
-                      children: <Widget>[
-                        Text("New folder"),
-                        Icon(
-                          Icons.library_add,
-                          color: Colors.amber
-                        )
-                      ],
-                    )
-                  )),
-                  hint: Text("Select folder"),
-                  onChanged: (index) {
-                      if(index == folders.length) {
-                        _showNewFolderFieldAlert(context)
-                            .then((newName) {
-                          if(newName != null) {
-                            print("New name: $newName");
-                            folders.add(newName);
-                            selectedIndex++;
-
-                          }
-                        });
-                      } else {
-                        selectedIndex = index;
-                        selectedFolder = folders[index];
-                      }
-                      print("index $selectedIndex selected");
-                  },
-                )
-              ],
-            )
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Done'),
-              onPressed: () {
-                Navigator.of(context).pop(selectedFolder);
-              },
-            ),
-            FlatButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop(selectedFolder);
-              },
-            )
-          ],
+        return DropdownDialog(
+          title: "Add to my stack",
+          items: folders,
+          onChange: (index) {}
         );
       }
     );
-  }
-
-  Future<String> _showNewFolderFieldAlert(BuildContext context) {
-    TextEditingController _controller = TextEditingController();
-    String newFolderName;
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Enter name"),
-            content: TextField(
-              autofocus: true,
-              onSubmitted: (text) {
-                newFolderName = _controller.text;
-                Navigator.of(context).pop(newFolderName);
-              },
-              controller: _controller,
-              cursorColor: Colors.amber,
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Ok'),
-                onPressed: () {
-                  newFolderName = _controller.text;
-
-                  Navigator.of(context).pop(newFolderName);
-                },
-              ),
-              FlatButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
   }
 }
