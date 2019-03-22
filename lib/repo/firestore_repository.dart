57f274e9646bde064d5c18b7c200914f4ee800e4
younny <younny@gmail.com>
2019-03-22 17:25:12 +0900

    Add custom exeption

--- lib/repo/firestore_repository.dart
+++ lib/repo/firestore_repository.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flipflop/constant/error.dart';
+import 'package:flipflop/exception.dart';
+import 'package:meta/meta.dart';
 
 class FirestoreRepository {
-  final Firestore _firestore;
+  Firestore _firestore;
 
   static final FirestoreRepository _instance = FirestoreRepository._internal(Firestore.instance);
 
@@ -32,16 +35,27 @@ class FirestoreRepository {
 
   Stream<QuerySnapshot> readByFilter(String collectionId, String filter) {
     List<String> parsedFilter = filter.split(':');
+    if(parsedFilter.isEmpty || parsedFilter.length < 4) {
+      throw FirestoreException("${FFError.FIRESTORE} Incorrect filter format.");
+    }
     String fieldOfFilter = parsedFilter[0];
     String valueOfFilter = parsedFilter[1].toLowerCase();
     int level = int.parse(parsedFilter[2]);
     String lang = parsedFilter[3];
-    return _firestore
-        .collection(collectionId)
-        .where(fieldOfFilter, isEqualTo: valueOfFilter.toLowerCase())
-        .where("level", isEqualTo: level)
-        .where("lang", isEqualTo: lang)
-        .snapshots();
+    try {
+      return _firestore
+          .collection(collectionId)
+          .where(fieldOfFilter, isEqualTo: valueOfFilter.toLowerCase())
+          .where("level", isEqualTo: level)
+          .where("lang", isEqualTo: lang)
+          .snapshots();
+    } catch (e) {
+      throw FirestoreException("${FFError.FIRESTORE} ${e.toString()}");
+    }
   }
 
+  @visibleForTesting
+  void withMock(Firestore firestore) {
+    _firestore = firestore;
+  }
 }
