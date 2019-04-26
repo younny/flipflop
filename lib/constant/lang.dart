15c8dc86ef46a6255d9c45de7402f9f7ddc97f5e
younny <younny@gmail.com>
2019-04-26 15:44:30 +0900

    Fix, get label function

--- lib/constant/lang.dart
+++ lib/constant/lang.dart
@@ -1,4 +1,11 @@
 class LangCode {
   static const String GERMAN = 'de';
   static const String KOREAN = 'ko';
+
+  static final Map _langLabels = {
+    KOREAN: 'Korean',
+    GERMAN: 'German'
+  };
+
+  static String label(String code) => _langLabels[code];
 }
